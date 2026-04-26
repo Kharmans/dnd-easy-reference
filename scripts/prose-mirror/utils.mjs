ae5250b06d9b62702105b539ac57033c04956f73
Kevin Garner <kevinleegarner@gmail.com>
2026-04-26 15:27:51 -0500

    Upgraded callouts to wrap the entire current block in callout markup if no selection is made and the current block has any text content.

--- scripts/prose-mirror/utils.mjs
+++ scripts/prose-mirror/utils.mjs
@@ -58,8 +58,8 @@ export function getSelectedText(menu) {
 
 /**
  * Slices the selection content into an array of nodes.
- * @param {any} menu          the ProseMirrorMenuInstance 
- * @returns {any[]|undefined} an array of nodes, if there is 
+ * @param {any} menu          the ProseMirrorMenuInstance
+ * @returns {any[]|undefined} an array of nodes, if there is
  *                            at least one node to return
  */
 export function sliceSelection(menu) {
@@ -77,12 +77,35 @@ export function sliceSelection(menu) {
 }
 
 /**
+ * Gets the topmost block node (just beneath root) related
+ * to the cursor's current location.
+ * @param {Object} params
+ * @param {any} params.menu
+ * @param {boolean} [params.selectBlockOnSuccess]  selects the block upon successfully finding a node.
+ *                                                    This is useful for preparing a block replacement.
+ * @returns {any[]|undefined}
+ */
+export function getBlockNodeFromCursorLocation({ menu, selectBlockOnSuccess }) {
+  const state = menu.view.state;
+  const node = state.selection.$head?.node(1);
+  const pos = state.selection.$from?.before(1);
+
+  if (node && selectBlockOnSuccess) {
+    const NodeSelection = foundry.prosemirror.state.NodeSelection;
+    const tr = state.tr.setSelection(NodeSelection.create(state.doc, pos));
+    menu.view.dispatch(tr);
+  }
+
+  return node ? [node] : undefined;
+}
+
+/**
  * Toggles a block around the current block of content. This functions
  * just like Secret Blocks, for example.
  * @param {Object} options        the options for toggling the block
- * @property {any} menu           the ProseMirrorMenu instance
- * @property {string} [class]     a class to apply when toggling the block
- * @property {string} type        the schema member (div, paragraph, aside, figure, etc.)
+ * @param {any} options.menu           the ProseMirrorMenu instance
+ * @param {string} [options.class]     a class to apply when toggling the block
+ * @param {string} options.type        the schema member (div, paragraph, aside, figure, etc.)
  *                                to use as the block
  */
 export function toggleBlock({ menu, class: cssClass, type }) {
