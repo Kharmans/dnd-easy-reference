65b7199d66ae3137c8cdd60a3caa9e50dbe8a5e7
Kevin Garner <kevinleegarner@gmail.com>
2026-05-08 20:43:23 -0500

    Fixed syntax error

--- scripts/pattern-detection/patterns.mjs
+++ scripts/pattern-detection/patterns.mjs
@@ -6,7 +6,7 @@ import { enDefinitions } from "./definitions/en.mjs";
 import { frDefinitions } from "./definitions/fr.mjs";
 
 /**
- * @type {Record<string, PatternDefinitions} Exported Pattern Definitions
+ * @type {Record<string, PatternDefinitions>} Exported Pattern Definitions
  */
 export const patternDefinitions = {
   en: enDefinitions,
